b48ca69d241142537c11bdaa283c8b54fe4ed0da
EC2 Default User <ec2-user@ip-172-31-45-140.ec2.internal>
2024-11-13 13:02:38 +0000

    code updated

--- flask_app.py
+++ flask_app.py
@@ -8,4 +8,4 @@ def welcome():
     return "<h1>Hello world</h1>"
 
 if __name__ == "__main__":
-    app.run((host='0.0.0.0', port=5000, debug=True))
+    app.run(host='0.0.0.0', port=5000, debug=True)
