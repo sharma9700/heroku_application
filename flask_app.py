000577353bf1ac762925f2f7d06f8e5559d81496
EC2 Default User <ec2-user@ip-172-31-45-140.ec2.internal>
2024-11-13 13:14:10 +0000

    code updated2

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,11 @@
 import flask
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("MyFirstApp")
 
 @app.route('/', methods=['GET'])
 def welcome():
     return "<h1>Hello world</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
