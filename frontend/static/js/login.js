e15707ee80193934510c4ec210f6fb90086bf623
EC2 Default User <ec2-user@ip-172-31-45-140.ec2.internal>
2024-12-16 13:56:15 +0000

    data

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
             switch (response.status) {
                 case 200:
-                    window.location.href = "{{ url_for('index.html') }}";; // Redirect on success
+                    window.location.href = "{{ url_for('aws.html') }}";; // Redirect on success
                     console.log(data.body || "Login Successful");
                     break;
                 case 400:
